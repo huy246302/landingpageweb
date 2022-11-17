27d8b2cfb4d09094abcc2d507179476eeaedd570
huynguyen24 <huy.nguyen@dgvdigital.com>
2022-11-17 23:12:22 +0700

    pagedetailid

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import * as React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
 import Home from "./pages/Home";
 import { ServicePage } from "./pages/Service/ServicePage";
 import { AboutPage } from "./pages/About/AboutPage";
@@ -10,6 +8,7 @@ import { PricingPage } from "./pages/Pricing/PricingPage";
 import { ItemPage } from "./pages/Item/ItemPage";
 import { LoginPage } from "./pages/Login/LoginPage";
 import { PageLayout} from "./pages/Layout/PageLayout";
+import { ItemDetailPage } from "./pages/Item/ItemDetailPage";
 
 function App() {
   return (
@@ -21,6 +20,7 @@ function App() {
           <Route path="/about" element={<AboutPage/>}/>
           <Route path="/pricing" element={<PricingPage/>}/>
           <Route path="/item" element={<ItemPage/>}/>
+          <Route path="/item/:id" element={<ItemDetailPage />} />
         </Route>
         <Route path="/login" element={<LoginPage />}/>
       </Routes>
---
+++ src/pages/Item/ItemDetailPage.js
@@ -0,0 +1,10 @@
+import {useParams} from "react-router-dom"
+import React from 'react'
+
+export function ItemDetailPage(props) {
+    const {id} = useParams();
+    console.log(id)
+    return (
+        <div>ItemDetailPage</div>
+    )
+}
